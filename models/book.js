98cbae37e98cc5fa80319940afdb5fa037667e58
Joseph Price <josephprice747@gmail.com>
2023-03-14 18:48:23 +0000

    add book.js, bookSchema

--- models/book.js
+++ models/book.js
@@ -3,8 +3,12 @@ const mongooses = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: string;
-  description: string;
+  title: String;
+  description: String;
   status: Boolean;
   
 })
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
