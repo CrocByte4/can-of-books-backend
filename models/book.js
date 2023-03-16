16df2ee5dfce57520f933ac007908d4566571318
Joseph Price <josephprice747@gmail.com>
2023-03-16 10:00:48 +0000

    add missing . on server line 5

--- models/book.js
+++ models/book.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: String;
-  description: String;
-  status: Boolean;
-  
-})
+  title: String,
+  description: String,
+  status: Boolean,
+});
 
 const Book = mongoose.model("Book", bookSchema);
 
